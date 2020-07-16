2b768632d62c14d14ea18b8146eae70b014c8e4d
habib256 <gist974@gmail.com>
2020-07-16 21:12:10 +0400

    socket things

--- thymiohttp.js
+++ thymiohttp.js
@@ -6,16 +6,16 @@ var server = app.listen(3000);
 app.use(express.static('dist'));
 console.log("My Thymio socket server is running at http://127.0.0.1:3000");
 
-var socket = require('socket.io');
-var io = socket(server);
+var socket;
+var io = require('socket.io').listen(server);
 io.sockets.on('connection',newConnection);
 
-function newConnection(socket) {
+function newConnection(_socket) {
+    socket = _socket;
     console.log('new connection: ' + socket.id);
     socket.on('thymio', thymioMsg);
 } 
 function thymioMsg(data) {
-  //io.socket.emit('Hello');
-   
-   console.log(data[2]);
+    socket.emit('Hello',6); 
+   //console.log(data[2]);
 }
