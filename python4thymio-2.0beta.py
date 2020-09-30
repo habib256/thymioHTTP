82591abd03800e542a621257885b2ff0dc96159f
habib256 <gist974@gmail.com>
2020-09-30 19:26:47 +0400

    Working basic python sketch

--- python4thymio-2.0beta.py
+++ python4thymio-2.0beta.py
@@ -1,18 +1,41 @@
-# ThymioHTTP Python interface
-# Vous aurez besoin d'installer request avec :
-# "pip install requests"
+# ThymioHTTP Python 3 API
+# Vous aurez besoin d'installer request avec : "pip install requests"
+# MIT License - VERHILLE Arnaud
 
+import json
+import time
 import requests
 
 response = requests.get ("http://127.0.0.1:3000/nodes")
 
+def avance(vitesse):
+    url = "http://127.0.0.1:3000/nodes/M_motor_both" + "/" + str(vitesse) + "/" + str(vitesse)
+    r = requests.put(url)
+    print (url)
+
+def moteurs(vg,vd):
+    url = "http://127.0.0.1:3000/nodes/M_motor_both" + "/" + str(vg) + "/" + str(vd)
+    r = requests.put(url)
+    print (url)
+
+def stop():
+    url = "http://127.0.0.1:3000/nodes/M_motor_both/0/0"
+    r = requests.put(url)
+    print (url)
+
 print ("Tableau de capteurs du Thymio :")
 print (response.json())
 
-# Update an existing resource
-requests.put('http://127.0.0.1:3000/nodes/ping', params = {'key':'value'})
+# Programmer le thymio en Python
 
+repetition = 10
+compteur = 0
 
-options = {'left':'200','right':'200' }
-r = requests.put('http://127.0.0.1:3000/nodes/M_motor_both', params = options )
-print(r.url)
+while (compteur < repetition) :
+    avance (300)
+    time.sleep(2)
+    moteurs(200,-200)
+    time.sleep(1.2)
+    compteur+=1
+    
+stop ()
