21ee504a0a75cee27005edad0fd31cef4c766892
habib256 <gist974@gmail.com>
2020-10-01 22:00:52 +0400

    suivi de ligne python3

--- python4thymio-2.0beta.py
+++ python4thymio-2.0beta.py
@@ -14,45 +14,68 @@ nodeurl = "http://127.0.0.1:3000/nodes"
 ################################################################
 # Fonctions pour accéder à l'interface HTTP REST de ThymioHTTP #
 ################################################################
+
+def lireCapteurs():
+    # Affiche les valeurs des capteurs du Thymio
+    response = requests.get (nodeurl+"/")
+    response = response.json()
+    thymioCapteurs = response[0][1]
+    return thymioCapteurs
+
 def avance(vitesse):
     url = nodeurl + "/M_motor_both" + "/" + str(vitesse) + "/" + str(vitesse)
     r = requests.put(url)
-    print (url)
 
 def moteurs(vg,vd):
     url = nodeurl + "/M_motor_both" + "/" + str(vg) + "/" + str(vd)
     r = requests.put(url)
-    print (url)
 
 def stop():
     url = nodeurl + "/M_motor_both/0/0"
     r = requests.put(url)
-    print (url)
 
 def ping():
     url = nodeurl + "/ping"
     r = requests.put(url)
-    print (url)
 
-#######################################
-# Programmation du thymio en Python 3 #
-#######################################
+##################################################
+# Exemples de programmes pour thymio en Python 3 #
+##################################################
 
-repetition = 10
-compteur = 0
+def danse1():
+    repetition = 4
+    compteur = 0
+    while (compteur < repetition) :
+        avance (300)
+        time.sleep(2)
+        ping()
+        moteurs(200,-200)
+        time.sleep(1.2)
+        ping()
+        compteur+=1 
+    stop ()
 
-while (compteur < repetition) :
-    avance (300)
-    time.sleep(2)
-    ping()
-    moteurs(200,-200)
-    time.sleep(1.2)
-    ping()
-    compteur+=1
-    
-stop ()
+def suiviDeLigne1():
+    while (True) :
+        capteurs = lireCapteurs()
+        time.sleep(0.1)
+        
+        #R_state[16] = prox.ground.delta[1]
+        if capteurs[16] > 400 :
+            moteurs(40,200)
+            time.sleep(0.1)
+        else :
+            #R_state[15] = prox.ground.delta[0]
+            if capteurs[15] > 400 :
+                moteurs(200,40)
+                time.sleep(0.1)
+            else :
+                moteurs(100,100)
+                time.sleep(0.1)
 
-# Affiche les valeurs des capteurs du Thymio
-response = requests.get (nodeurl)
-print ("Tableau de capteurs du Thymio :")
-print (response.json())
+
+#############################################
+# Programme principal du thymio en Python 3 #
+#############################################
+
+suiviDeLigne1()
