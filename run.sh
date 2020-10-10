a26badf1042908d9304bc08c0445b67b760cfc44
habib256 <gist974@gmail.com>
2020-10-10 20:51:22 +0400

    Simplier starting script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 cd /opt/thymioHTTP
-npm start
+
+/var/lib/flatpak/app/org.mobsya.ThymioSuite/current/active/files/bin/thymio-device-manager > /dev/null &
+node thymiohttp.js & 
+chromium-browser 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=snap4thymio-2.1&editMode&noRun"
+
