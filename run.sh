da40bbaf0b67f7efe0af06ea6b473cee260d0cc6
habib256 <gist974@gmail.com>
2020-10-10 22:17:50 +0400

    Firefox is the best

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ cd /opt/thymioHTTP
 
 /var/lib/flatpak/app/org.mobsya.ThymioSuite/current/active/files/bin/thymio-device-manager > /dev/null &
 node thymiohttp.js & 
-chromium-browser 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=snap4thymio-2.1&editMode&noRun"
+#chromium-browser 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=snap4thymio-2.1&editMode&noRun"
+firefox 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=snap4thymio-2.1&editMode&noRun"
 
