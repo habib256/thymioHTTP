7be26f2f4df696b70b34cf7e09aa11f675517d87
habib256 <gist974@gmail.com>
2021-01-13 17:14:50 +0400

    Snap! to ThymioHTTP Code Transfert work :-)

--- thymiohttp.js
+++ thymiohttp.js
@@ -19,8 +19,6 @@ app.use(bodyParser.text())
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
-
 var data = [0];
 
 var socket;
