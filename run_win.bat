dd4437400284ea7874fa5a2e1ab1a3efe3a713d0
habib256 <gist974@gmail.com>
2021-01-25 22:59:15 +0400

    Autostart for windows

--- run_win.bat
+++ run_win.bat
@@ -1,3 +1,4 @@
+rem AUTOSTART ThymioHTTP under Windows 
 
 cd "C:\Program Files\Thymio\bin"
 start /MIN thymio-device-manager
@@ -5,4 +6,7 @@ start /MIN thymio-device-manager
 cd C:\Users\gistarcade\src\thymioHTTP
 start /MIN node thymiohttp.js
 
-C:\"Program Files\Mozilla Firefox\firefox.exe" 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=basic_thymio_motion.aesl&editMode&noRun"
+C:\"Program Files\Mozilla Firefox\firefox.exe" "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=basic_thymio_motion.aesl&editMode&noRun" 127.0.0.1:3000
+
+rem C:\"Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=basic_thymio_motion.aesl&editMode&noRun" 127.0.0.1:3000
+
