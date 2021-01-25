93ea73ac40bd9d76dec76e6a49647ca9ba459bc4
habib256 <gist974@gmail.com>
2021-01-25 22:15:12 +0400

    Playing with win10 autostart script

---
+++ run_win.bat
@@ -0,0 +1,8 @@
+
+cd "C:\Program Files\Thymio\bin"
+start /MIN thymio-device-manager
+
+cd C:\Users\gistarcade\src\thymioHTTP
+start /MIN node thymiohttp.js
+
+C:\"Program Files\Mozilla Firefox\firefox.exe" 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=basic_thymio_motion.aesl&editMode&noRun"
