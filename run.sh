65a9d090d912fcdd32e5546cf72411d40642cb69
habib256 <gist974@gmail.com>
2021-01-25 23:04:47 +0400

    Better autorun GNU/Linux

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ node thymiohttp.js &
 #chromium-browser 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=snap4thymio-2.1&editMode&noRun"
 #firefox 127.0.0.1:3000 "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snaptoaseba.xml&editMode&noRun"
 
-chromium-browser 127.0.0.1:3000 "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snap4thymio.xml&editMode&noRun"
+chromium-browser  "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snap4thymio.xml&editMode&noRun" 127.0.0.1:3000
 
