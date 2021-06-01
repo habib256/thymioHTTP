921bb00ce190936a0c44a1d57d4625e3d3750ef6
habib256 <gist974@gmail.com>
2021-06-01 23:45:51 +0400

    google chrome 91 new Javascript V8

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ node thymiohttp.js &
 #chromium-browser 127.0.0.1:3000 "https://snap.berkeley.edu/snap/snap.html#present:Username=gist&ProjectName=snap4thymio-2.1&editMode&noRun"
 #firefox 127.0.0.1:3000 "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snaptoaseba.xml&editMode&noRun"
 
-chromium-browser  "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snap4thymio.xml&editMode&noRun" 127.0.0.1:3000
-chromium  "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snap4thymio.xml&editMode&noRun" 127.0.0.1:3000
+#chromium-browser  "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snap4thymio.xml&editMode&noRun" 127.0.0.1:3000
+google-chrome-stable  "file:///opt/Snap/snap.html#run:http://127.0.0.1:3000/snap4thymio.xml&editMode&noRun" 127.0.0.1:3000
 
